07d8a829e16f4c4884d48946d1a7b88dfede4b97
praveendareddy21 <praveendareddy@gmail.com>
2017-03-07 23:43:37 -0800

    added another requirement to second test case

--- auto_test.cpp
+++ auto_test.cpp
@@ -298,7 +298,7 @@ void TestPlayerCanMoveToEmpty::doTest(){
 		read(c1_to_p[C1_to_P_READ_END],&p1_drawMapCount,sizeof(int));
 		read(c1_to_p[C1_to_P_READ_END],map,rows*cols);
 
-		if(map[5] & G_PLR0) //&& !(map[4] & G_PLR0))
+		if(map[5] & G_PLR0 && p1_drawMapCount == 1) //&& !(map[4] & G_PLR0))
 		{
 			test_result =  true;
 			cout<<"in 4"<<endl;
@@ -316,7 +316,7 @@ void TestPlayerCanMoveToEmpty::doTest(){
 		read(c1_to_p[C1_to_P_READ_END],&p1_drawMapCount,sizeof(int));
 		read(c1_to_p[C1_to_P_READ_END],map,rows*cols);
 
-		if(map[4] & G_PLR0 ) //&& !(map[5] & G_PLR0))
+		if(map[4] & G_PLR0 && p1_drawMapCount == 1 ) //&& !(map[5] & G_PLR0))
 		{
 			test_result =  true;
 			cout<<"in 5"<<endl;
@@ -351,20 +351,17 @@ void TestOnlyRightKey::doTest(){
 }
 int main(){
 	//TestPlayerCanMoveToEmpty b(3, 5, "2\n*****\n**  *\n*** *", 1);
-	TestPlayerCanMoveToEmpty t2(3, 3, "0\n***\n*  \n***", 1);
-	t2.doTest();
-	t2.cleanUpTestEnv();
 
 
-	return 0;
+
 
 	TestPlayerCantMoveIntoWall t1(3, 3, "0\n***\n* *\n***", 1);
 	t1.doTest();
 	t1.cleanUpTestEnv();
 
-
-
-
+	TestPlayerCanMoveToEmpty t2(3, 3, "0\n***\n*  \n***", 1);
+	t2.doTest();
+	t2.cleanUpTestEnv();
 
 	return 0;
 
